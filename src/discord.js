8ecc1168462f766a5b3ae1d5dbc38d94d388a024
sandyagur8 <santiago.appu@gmail.com>
2023-09-24 15:12:50 +0530

    Added all protocols starkknet remaining

--- src/discord.js
+++ src/discord.js
@@ -2,14 +2,19 @@ import csv from 'csvtojson'
 import fs from 'fs'
 
 /* Change this file path to csv file name */
-const csvFilePath='discord_raw_chats/flashbots ethglobal.csv'
+const filesArray=['celo0.csv', 'celo1.csv', 'celo2.csv']
+const nameSplit= filesArray[0].split(".")
+for(let i=0;i<filesArray.length;i++){
+    const csvFilePath='discord_raw_chats/'+filesArray[i]
+    console.log(csvFilePath)
+    let finalString
 
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
     let finalJSON = {messages:[]}
-    let finalString
     jsonObj.map(obj=>{
+        
         const {Username, Content} = obj
         const str = `${obj?.Username} says ${obj?.Content}`
         // finalJSON.messages.push(
@@ -20,8 +25,11 @@ csv()
     })
     console.log(finalString)
     // console.log(finalJSON)
-    // fs.writeFileSync('discord_output/output.txt', finalString); 
-    fs.writeFileSync('discord_output/chatgpt/flashbots.json', finalString); 
+    // fs.writeFileSync('discord_output/chatgpt/Biconomy1.json', finalString); 
+    fs.writeFileSync('./discord_output/'+nameSplit[0]+'.txt', String(finalString + "\n"),{flag:"a+"});
 })
+}
+ 
+
 
 
--- src/gpt-bot/index.js
+++ src/gpt-bot/index.js
@@ -33,6 +33,20 @@ client.on('messageCreate', async (message)=>{
         '1inch':"f829e92e-92de-4c1a-8f43-1f072046b86b",
         'flashbots':"e5247c99-3f85-4336-bbf9-bc062fc3fed7",
         'uma':"b6932206-364d-4db1-84f2-7bdb6189bc1d",
+        'near':'4d11bbfb-6f88-4a91-9505-d95d535e39d3',
+        'tba':'c0e788b0-55d3-4dc2-8e38-abdf8086437f',
+        'hyperlane':'65c67eb1-a336-4218-b500-cae85163db7c',
+        'graph':'1fc10d26-b5e7-49d7-9a42-35fed8ad3c22',
+        'neon':'47c9a119-1528-4d0d-9506-8cdef27673ac',
+        'base':'8f0a47bb-d472-4eb1-a922-d6bc6aba0a2d',
+        'celo':'14810c18-07f4-41ca-9acb-ea2e42d6cb82',
+        'axelar':'8e4bd513-74e7-432f-8ccc-d1708f68cc57',
+        'arbitrum':'45099d5d-f999-4eb3-ab10-5e631ab4586c',
+        'cartesi':'51c066db-7d27-4267-afc9-dfd66e18ce0b',
+        'safe':'95e55ff0-f3fe-4b7c-9240-320e93ac7c3e',
+        'biconomy':'70fd3b41-2938-48a9-bb19-0eda26172846',
+        'starknet':''
+
     }
 
 
