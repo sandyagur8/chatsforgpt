b3588442ac67d61cccec6fb95c911e0d9db93d93
Pranay <zeppez@protonmail.com>
2023-09-23 20:45:37 -0400

    hlh

--- src/discord.js
+++ src/discord.js
@@ -1,26 +1,27 @@
-const csv = require('csvtojson')
-const fs = require('fs') 
+import csv from 'csvtojson'
+import fs from 'fs'
 
 /* Change this file path to csv file name */
-const csvFilePath='discord_raw_chats/Apecoin ethglobal.csv'
+const csvFilePath='discord_raw_chats/flashbots ethglobal.csv'
 
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
     let finalJSON = {messages:[]}
+    let finalString
     jsonObj.map(obj=>{
         const {Username, Content} = obj
-        // const str = `${obj?.Username} says ${obj?.Content}`
-        finalJSON.messages.push(
-            {"role": Username, "content": Content}
-        )
+        const str = `${obj?.Username} says ${obj?.Content}`
+        // finalJSON.messages.push(
+            // {"role": Username, "content": Content}
+        // )
         // console.log(str);
-        // finalString = finalString + '\n' + str
+        finalString = finalString + '\n' + str
     })
-    // console.log(finalString)
-    console.log(finalJSON)
+    console.log(finalString)
+    // console.log(finalJSON)
     // fs.writeFileSync('discord_output/output.txt', finalString); 
-    fs.writeFileSync('discord_output/chatgpt/output.json', JSON.stringify(finalJSON)); 
+    fs.writeFileSync('discord_output/chatgpt/flashbots.json', finalString); 
 })
 
 
